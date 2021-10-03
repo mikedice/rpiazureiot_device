bddf442d2c181d5e3560749b0e3d98be0dc4f6f4
mikedice <mikedice@microsoft.com>
2021-10-03 13:56:46 -0700

    Add files

--- runlocal.sh
+++ runlocal.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# localenv.sh exports an environment variable 'HubConnectionString'
+# that exposes the Azure connection string for the IoT device. The localenv.sh
+# script is not part of the repo because it contains the connection string,
+# which is a secret.
 source /home/pi/code/js/azureiot_device/localenv.sh
 /home/pi/bin/node /home/pi/code/js/azureiot_device/app.js
 
